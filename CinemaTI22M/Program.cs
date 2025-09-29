bd7d70082d5cf648a3a96de0a9855b5fad272998
CAIO DE SOUSA VIEIRA <caio.svieira2@senacsp.edu.br>
2025-09-29 11:47:56 -0300

    fourth commit

---
+++ CinemaTI22M/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTI22M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }//Fim do método
+    }//Fim da classe
+}//Fim do projeto
